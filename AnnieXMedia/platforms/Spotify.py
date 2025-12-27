78d1ed510cbf7adf114d78035cc498cb81150f87
mmahsunaz-eng <mmahsun.az@gmail.com>
2025-12-28 03:55:13 +0700

    Update Spotify.py

--- AnnieXMedia/platforms/Spotify.py
+++ AnnieXMedia/platforms/Spotify.py
@@ -3,7 +3,7 @@ import re
 
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
-from youtubesearchpython.__future__ import VideosSearch
+from youtubesearchpython.__aio__ import VideosSearch
 
 import config
 
