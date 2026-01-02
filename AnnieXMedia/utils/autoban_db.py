004e996d7c753bd895d687ad103eb64ce5340d0f
mmahsunaz-eng <mmahsun.az@gmail.com>
2026-01-02 11:51:43 +0700

    Update autoban_db.py

--- AnnieXMedia/utils/autoban_db.py
+++ AnnieXMedia/utils/autoban_db.py
@@ -1,51 +1,45 @@
-import os
-from motor.motor_asyncio import AsyncIOMotorClient
+from AnnieXMedia.core.mongo import mongodb
 
-mongo = AsyncIOMotorClient(os.getenv("MONGO_DB_URI"))
-db = Annie
-col = db.autoban_words
+autoban = mongodb.autoban
+
 
 async def get_chat(chat_id: int):
-    data = await col.find_one({"chat_id": chat_id})
-    if not data:
-        data = {
-            "chat_id": chat_id,
-            "enabled": True,
-            "words": [],
-            "whitelist": []
-        }
-        await col.insert_one(data)
-    return data
+    return await autoban.find_one({"chat_id": chat_id})
 
-async def add_word(chat_id: int, word: str):
-    await col.update_one(
-        {"chat_id": chat_id},
-        {"$addToSet": {"words": word}},
-        upsert=True
-    )
-
-async def remove_word(chat_id: int, word: str):
-    await col.update_one(
-        {"chat_id": chat_id},
-        {"$pull": {"words": word}}
-    )
 
 async def set_status(chat_id: int, status: bool):
-    await col.update_one(
+    await autoban.update_one(
         {"chat_id": chat_id},
         {"$set": {"enabled": status}},
         upsert=True
     )
 
+
+async def add_word(chat_id: int, word: str):
+    await autoban.update_one(
+        {"chat_id": chat_id},
+        {"$addToSet": {"words": word}},
+        upsert=True
+    )
+
+
+async def remove_word(chat_id: int, word: str):
+    await autoban.update_one(
+        {"chat_id": chat_id},
+        {"$pull": {"words": word}}
+    )
+
+
 async def add_whitelist(chat_id: int, user_id: int):
-    await col.update_one(
+    await autoban.update_one(
         {"chat_id": chat_id},
         {"$addToSet": {"whitelist": user_id}},
         upsert=True
     )
 
+
 async def remove_whitelist(chat_id: int, user_id: int):
-    await col.update_one(
+    await autoban.update_one(
         {"chat_id": chat_id},
         {"$pull": {"whitelist": user_id}}
-    )
+)
