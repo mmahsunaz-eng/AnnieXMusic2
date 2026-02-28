4e2b85311a2ef3200f431d5e6f889ee9a8fe7f5f
mmahsunaz-eng <mmahsun.az@gmail.com>
2026-03-01 04:28:25 +0700

    Update downloader.py

--- AnnieXMedia/utils/downloader.py
+++ AnnieXMedia/utils/downloader.py
@@ -88,7 +88,7 @@ def get_ytdlp_base_opts() -> Dict[str, object]:
         # 🔥 WAJIB: bypass throttling YouTube
         "extractor_args": {
             "youtube": {
-                "player_client": ["android", "web"]
+                "player_client": ["tv", "android", "web"]
             }
         },
 
@@ -310,7 +310,7 @@ async def yt_dlp_download(link: str, type: str, title: str = "") -> Optional[str
         async def run():
             ytdlp_task = asyncio.create_task(
                 run_with_semaphore(
-                    loop.run_in_executor(None, download_with_ytdlp_sync, link, "bestaudio[acodec!=none]/best")
+                    loop.run_in_executor(None, download_with_ytdlp_sync, link, "bestaudio/best")
                 )
             )
             api_task = asyncio.create_task(api_download_audio(link)) if USE_AUDIO_API else None
