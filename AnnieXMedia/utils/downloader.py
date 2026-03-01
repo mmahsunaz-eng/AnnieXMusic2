93cfd28d4973eee1fcea682a1b0e49ed21864b87
mmahsunaz-eng <mmahsun.az@gmail.com>
2026-03-01 13:55:18 +0700

    Update downloader.py

--- AnnieXMedia/utils/downloader.py
+++ AnnieXMedia/utils/downloader.py
@@ -279,7 +279,7 @@ async def yt_dlp_download(link: str, type: str, title: str = "") -> Optional[str
         async def run():
             ytdlp_task = asyncio.create_task(
                 run_with_semaphore(
-                    loop.run_in_executor(None, download_with_ytdlp_sync, link, bestaudio[ext=m4a]/bestaudio[ext=webm]/bestaudio/best)
+                    loop.run_in_executor(None, download_with_ytdlp_sync, link, "bestaudio[ext=m4a]/bestaudio[ext=webm]/bestaudio/best")
                 )
             )
             api_task = asyncio.create_task(api_download_audio(link)) if USE_AUDIO_API else None
