9b755a59a9084116afbf3e1f37e8a8350498aa58
mmahsunaz-eng <mmahsun.az@gmail.com>
2026-04-16 21:59:52 +0700

    Update downloader.py

--- AnnieXMedia/utils/downloader.py
+++ AnnieXMedia/utils/downloader.py
@@ -103,7 +103,7 @@ def get_ytdlp_base_opts() -> Dict[str, object]:
         "extractor_args": {
            "youtube": {
                "player_client": 
-        ["android"," tv"], 
+        ["android","tv"], 
                "formats": ["missing_pot"],
                "player_skip": ["webpage", "configs"]
                     }
